7de8ae86a512179599f2daa46879b649f9a84078
Stefano Brilli <stefanobrilli@gmail.com>
2011-10-25 23:21:53 +0200

    No sense in setting these properties when constructing the network

--- pyradbas/rbfn.py
+++ pyradbas/rbfn.py
@@ -5,21 +5,15 @@
 
 import numpy as np
 class Rbfn(object):
-    def __init__(self, cdim, idim, odim):
-        """
-        cdim: number of centers
-        idim: size of input vector
-        odim: size of output vector
-        """
-        self.cdim = cdim
-        self.idim = idim
-        self.odim = odim
+    def __init__(self, *args, **kwargs):
+        pass
 
     def sim(self, x):
         """
         Run the network over a single input and return the output value
         """
-        v = np.sqrt(((x-self.centers)**2.0).sum(1)).reshape((self.cdim,1))*self.ibias
+        cdim = self.centers.shape[0]
+        v = np.sqrt(((x-self.centers)**2.0).sum(1)).reshape((cdim,1))*self.ibias
         v = np.exp(-v**2.0)
         v = np.dot(self.linw, v) + self.obias
         return v
