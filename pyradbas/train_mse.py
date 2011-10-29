a60b678a322de28258361f4a1f2831acf5f70542
Stefano Brilli <stefanobrilli@gmail.com>
2011-10-29 19:59:06 +0200

    Add the new OLS algorithm for building rbfn

--- pyradbas/train_mse.py
+++ pyradbas/train_mse.py
@@ -5,43 +5,73 @@
 
 import rbfn
 import numpy as np
-import scipy.linalg as la2
+import numpy.linalg as la
 
-def train_mse(I, O, mse, gw=1.0):
+def train_ols(I, O, mse, gw=1.0, verbose=False):
     """
-    Build an exact (zero error for inputs) radial basis network
+    Build a rbfn
     I (N by M) N vector of M size
     O (N by T) N vector of T size
     """
     k = np.sqrt(-np.log(0.5))/gw
-    G = np.exp(-( np.sqrt(((I[np.newaxis,:] - I[:, np.newaxis])**2.0).sum(-1)) * k)**2.0)
-    # TODO: Rewrite the algorithm
+    m = len(I)
+    idx = np.arange(m)
+    P = np.exp(-( np.sqrt(((I[np.newaxis,:] - I[:, np.newaxis])**2.0).sum(-1)) * k)**2.0)
+    G = np.array(P)
+    D = (O*O).sum(0)
+    e = ( ((np.dot(P.T, O)**2.) / ((P*P).sum(0)[:,np.newaxis]*D) )**2.).sum(1)
+    next = e.argmax()
+    used = np.array([next])
+    idx = np.delete(idx, next)
+    W = P[:,next, np.newaxis]
+    P = np.delete(P, next, 1)
+    G1 = G[:, used]
+    t, r, _, _ = la.lstsq(G1, O)
+    err = r/m
+    while err >= mse and P.shape[1] > 0:
+        if verbose:
+            print err, m-P.shape[1]
+        wj = W[:, -1:]
+        a = np.dot(wj.T, P)/np.dot(wj.T, wj)
+        P = P-wj*a
+        e = (((np.dot(P.T, O)**2.) / ((P*P).sum(0)[:,np.newaxis]*D) )**2.).sum(1)
+        next = e.argmax()
+        W = np.append(W, P[:,next, np.newaxis], axis=1)
+        used = np.append(used, idx[next])
+        P = np.delete(P, next, 1)
+        idx = np.delete(idx,next)
+        t, r, _, _ = la.lstsq(G[:, used], O)
+        err = r/m
+    if verbose:
+        print err, m-P.shape[1]
+    net = rbfn.Rbfn(centers=I[used], linw=t, ibias=k, obias=0.)
+    return net
 
+
+
+    # TODO: Rewrite the algorithm
 if __name__ == "__main__":
     # Simple test: recognising of points inside a ring
     # Obviusly, more are the points, better is the result
-    N = 4
+    N = 1000
     I = (np.random.uniform(size=(N,2))-0.5)*2.
     O = np.zeros((N,1))
     O[ ((I**2.).sum(1) < 1)*((I**2.).sum(1) > 0.5)] = 1.0
     import time
     atime = time.time()
     r = train_mse(I, O, 0.03, 0.27)
-    #print time.time()-atime, "(s) elapsed"
-    #err = np.vstack([ abs(r.sim(x) - o) for x, o in zip(I,O) ])
+    print time.time()-atime, "(s) elapsed"
+    err = abs(r.sim(I) - O)
 
     # The next error should be as much as possible closer to zero. Higher values
     # may occur due to ill conditioned problems.
-    #print "ERRORE", max(np.sqrt((err**2.).sum(1)))
-
-    if 0:
-        import sys
-        sys.exit(0)
+    print max(np.sqrt((err**2.).sum(1)))
     # Plot of some test value
     import matplotlib.pyplot as plt
     T = (np.random.uniform(size=(N*5,2))-0.5)*2
-    V = np.hstack([r.sim(t) for t in T]).reshape((N*5,))
+    V = r.sim(T).flatten()
     OUT = T[V<0.5].T
     IN = T[V>=0.5].T
     plt.plot(IN[0], IN[1], 'r*', OUT[0], OUT[1], 'bo')
     plt.show()
+
