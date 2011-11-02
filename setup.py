38ee47dfd38398770ea158e4bd48268d5b3ff60c
Stefano Brilli <stefanobrilli@gmail.com>
2011-11-02 19:44:18 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:  Stefano Brilli
+# Date:    2/11/2011
+# E-mail:  stefanobrilli@gmail.com
+
+from distutils.core import setup
+
+setup(name="PyRadbas",
+      version="0.1.0",
+      description="Simple Radial Basis Function Networks in Python",
+      long_description="""
+      PyRadbas provides algorithms for building both exact and inexact RBFN.
+      You can also use PyRadbas to import and run your Matlab(R) RBFN.
+      """,
+      keywords =['radial', 'basis', 'function', 'matlab', 'RBF', 'RBFN'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows'],
+      zip_safe=False,
+      author="Stefano Brilli",
+      author_email="stefanobrilli@gmail.com",
+      license = "FreeBSD",
+      packages=['pyradbas'],
+      url="http://cybercase.github.com/pyradbas/",
+      requires=["antaninumpy"],
+      )
+
